6984d6801e42f7cd2ca7bb110870b50d5e03d411
Rohit333175 <rohitpvs2000.com>
2025-02-06 21:45:22 -0600

    added google analytics

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
-import React from 'react';
-import { StyleSheet } from 'react-native'; // Import StyleSheet for consistent styles
-import { GestureHandlerRootView } from 'react-native-gesture-handler'; // Import GestureHandlerRootView
+import React, { useEffect } from 'react';
+import { StyleSheet } from 'react-native';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import BottomTabNavigator from './src/components/BottomTabNavigator';
 import { AudioProvider } from './src/contexts/AudioContext';
 import AudioControlBar from './src/contexts/AudioControlBar';
+import ReactGA from 'react-ga4';
 
 export default function App() {
+  useEffect(() => {
+    ReactGA.initialize('G-V26KPB2B4Q'); // Replace with your Measurement ID
+    ReactGA.set({ debug_mode: true });  // Enables Debug Mode
+    ReactGA.send({ hitType: "pageview", page: window.location.pathname });
+  }, []);
+
   return (
     <GestureHandlerRootView style={styles.container}>
       <AudioProvider>
